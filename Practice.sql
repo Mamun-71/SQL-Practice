bdc305858b3c6fbb8b6768e3f16c80046ce44507
Al-Mamun Salauddin <63588230+Mamun-71@users.noreply.github.com>
2023-10-19 11:00:24 +0600

    Update Practice.sql

--- Practice.sql
+++ Practice.sql
@@ -23,4 +23,49 @@ create table student
 roolno INT primary key,
 name varchar(50)
 );
+
+
+create database college;
+use college;
+
+create table student
+(
+roolno INT primary key,
+name varchar(50)
+);
+
+select * from student;
+
+ insert into student
+ (roolno,name)
+ values
+ (101,"mamun"),
+ (102,"samin"),
+ (103,"rasel");
  
+ insert into student values (108,"emtias");
+ 
+ create database rokomari;
+ use rokomari;
+ 
+ create table employee
+ (
+  id int primary key,
+  name varchar(50),
+  salary int
+);
+
+insert into employee
+(id,name,salary)
+values
+(201,"mamun",20000),
+(202,"samin",50000);
+
+insert into employee values(203,"rasel",35000);
+
+
+select * from employee;
+
+ 
+
+
