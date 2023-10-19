11adf8d5b4a8bfaed0a74422975b629b9d29cc9c
Al-Mamun Salauddin <63588230+Mamun-71@users.noreply.github.com>
2023-10-19 11:05:43 +0600

    Update Practice.sql

--- Practice.sql
+++ Practice.sql
@@ -1,22 +1,25 @@
+-- ****--
 create database college;
--- create database if not exists college;
+ create database if not exists college;
 
--- drop database if exists company;
+ drop database if exists company;
 
--- use college;
+ use college;
 
--- create table student(
---   id int primary key,
---   name varchar(50),
---   age int not null
--- );
+ create table student(
+   id int primary key,
+   name varchar(50),
+   age int not null
+ );
 
--- insert into student values(1,"Mamun",26);
--- insert into student values(2,"Samin",25);
+ insert into student values(1,"Mamun",26);
+ insert into student values(2,"Samin",25);
 
--- select * from student;
+ select * from student;
 
--- show databases;
+ show databases;
+
+-- ****--
 
 create table student
 (
@@ -65,6 +68,10 @@ insert into employee values(203,"rasel",35000);
 
 
 select * from employee;
+
+--****--
+
+
 
  
 
